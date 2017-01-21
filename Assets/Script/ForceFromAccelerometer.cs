2718ef1cd7abee62fb9d3cbaae6553be4143a111
Luca Neri <lucasmodzita@gmail.com>
2017-01-21 16:40:45 +0100

    - Fisica

--- Assets/Script/ForceFromAccelerometer.cs
+++ Assets/Script/ForceFromAccelerometer.cs
@@ -3,22 +3,36 @@
 [RequireComponent(typeof(Rigidbody2D))]
 public class ForceFromAccelerometer : MonoBehaviour
 {
+    [Range(2,8)]
     public float maxVelocity = 5;
+    
+    [Range(20,100)]
+    public float reactivity = 60;
 
     private Rigidbody2D rb;
 
-    private const float GRAVITY = 50;
+    public UnityEngine.UI.Text text;
+
     private float gx, gy;
+
+    Quaternion currentRotation;
     
     private void Awake()
     {
+        Input.gyro.enabled = true;
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+
         rb = this.GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
-        gx = Input.acceleration.x * GRAVITY;
-        gy = Input.acceleration.y * GRAVITY;
+        currentRotation = Input.gyro.attitude;
+
+        text.text = Input.acceleration.x.ToString();
+
+        gx = Input.acceleration.x * reactivity;
+        gy = Input.acceleration.y * reactivity;
     }
 
     private void FixedUpdate()
