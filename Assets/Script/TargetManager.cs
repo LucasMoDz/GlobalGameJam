8771ad2a925ba3843c5c4548880e0d840f8de52d
Daniele Sangineto <scidros@gmail.com>
2017-01-21 18:40:25 +0100

    - SCRIPT : TargetManager

---
+++ Assets/Script/TargetManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetManager : MonoBehaviour {
+
+    WaveBarHandler wabaha;
+	
+	void Start ()
+    {
+        wabaha = FindObjectOfType<WaveBarHandler>();
+        SetCount();
+	}
+	
+	
+	void SetCount ()
+    {
+        int num = Random.Range(-15, 15);
+        wabaha.count += num;
+        
+	}
+}
