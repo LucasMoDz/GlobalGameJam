c680968addc3670f3b94ecb2b8fd840ba46241e1
Andrea Mercuri <andrea.mercuri21@gmail.com>
2017-01-21 19:43:47 +0100

    - Start Screen - Prefab fader

---
+++ Assets/Script/CanvasFader.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CanvasFader : MonoBehaviour
+{
+
+	public float fadeValue = 0.02f;
+	public bool fadeOnStart;
+
+	public void Start(){
+		if (fadeOnStart) {
+			this.GetComponent<CanvasGroup>().alpha = 1;
+			FadeOut();
+		}
+	}
+
+    public void FadeIn()
+    {
+        StartCoroutine(FadeInCO());
+    }
+
+    public void FadeOut()
+    {
+        StartCoroutine(FadeOutCO());
+    }
+
+    private IEnumerator FadeInCO()
+    {
+		while (this.gameObject.GetComponent<CanvasGroup>().alpha < 1)
+        {
+			this.gameObject.GetComponent<CanvasGroup>().alpha += fadeValue;
+            yield return null;
+        }
+
+        yield break;
+    }
+
+    private IEnumerator FadeOutCO()
+    {
+		while (this.gameObject.GetComponent<CanvasGroup>().alpha > 0)
+        {
+			this.gameObject.GetComponent<CanvasGroup>().alpha -= fadeValue;
+            yield return null;
+        }
+
+        yield break;
+    }
+}
---
+++ Assets/Script/LoadScene.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+	public GameObject CanvasFade;
+	public string sceneToLoad;
+
+	public void Start () {
+		CanvasFade.GetComponent<CanvasGroup> ().alpha = 1;
+		CanvasFade.GetComponent<CanvasFader> ().FadeOut ();
+	}
+
+	public void LoadNewGame ()
+	{
+		StartCoroutine (FadeToScene ());
+
+	}
+		
+
+	public IEnumerator FadeToScene () {
+		if (CanvasFade != null)
+		{
+			CanvasFade.GetComponent<CanvasFader>().FadeIn();
+			yield return new WaitForSeconds(0.8f);
+			SceneManager.LoadScene(sceneToLoad);
+		}
+
+
+	}
+} 
