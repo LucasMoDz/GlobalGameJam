b548652bc175dbc8a65de2cc4605b97fbd74606f
Daniele Sangineto <scidros@gmail.com>
2017-01-21 22:39:15 +0100

    - Modifiche

--- Assets/Script/TargetManager.cs
+++ Assets/Script/TargetManager.cs
@@ -15,7 +15,27 @@ public class TargetManager : MonoBehaviour {
 	void SetCount ()
     {
         int num = Random.Range(-15, 16);
-        wabaha.count += num;
-        
-	}
+        int range = wabaha.count += num;
+
+        if(range >=0 && range <= 5)
+            wabaha.UpdateTarget(1);
+
+        else if (range >= 6 && range <= 11)
+            wabaha.UpdateTarget(2);
+
+        else if (range >= 12 && range <= 17)
+            wabaha.UpdateTarget(3);
+
+        else if (range >= 18 && range <= 23)
+            wabaha.UpdateTarget(4);
+
+        else if (range >= 24 && range <= 30)
+            wabaha.UpdateTarget(5);
+
+
+
+
+
+    }
+
 }
--- Assets/Script/WaveBarHandler.cs
+++ Assets/Script/WaveBarHandler.cs
@@ -4,11 +4,15 @@ public class WaveBarHandler : MonoBehaviour
 {
 	private RectTransform rectArrow;
 	public int count = 15;
-    
-	private void Awake()
+    public RectTransform rectTarget;
+    private RectTransform rectTargetFather;
+
+    private void Awake()
     {
 		rectArrow = this.transform.parent.gameObject.GetComponent<RectTransform>();
-	}
+        rectTarget = rectTarget.transform.gameObject.GetComponent<RectTransform>();
+        rectTargetFather = rectTarget.transform.parent.gameObject.GetComponent<RectTransform>();
+    }
 	
 	public void UpdateBar(int _value)
     {
@@ -30,5 +34,50 @@ public class WaveBarHandler : MonoBehaviour
             this.transform.localPosition = new Vector3(this.transform.localPosition.x, rectArrow.sizeDelta.y / 2, this.transform.localPosition.z);
             count = 30;
         }
+    }
+
+    private void CheckTargetLimits()
+    {
+        if (count <= 0)
+        {
+            rectTarget.transform.localPosition = new Vector3(rectTarget.transform.localPosition.x, -rectTargetFather.sizeDelta.y / 2, rectTarget.transform.localPosition.z);
+            count = 0;
+        }
+        else if (count >= 30)
+        {
+            rectTarget.transform.localPosition = new Vector3(rectTarget.transform.localPosition.x, rectTargetFather.sizeDelta.y / 2, rectTarget.transform.localPosition.z);
+            count = 30;
+        }
+    }
+
+    public void UpdateTarget(int _value)
+    {
+        switch (_value)
+        {
+            case 1:
+            rectTarget.transform.localPosition += new Vector3(0, rectTargetFather.sizeDelta.y / 5 * _value, 0);
+                break;
+
+            case 2:
+                rectTarget.transform.localPosition += new Vector3(0, rectTargetFather.sizeDelta.y / 5 * _value, 0);
+                break;
+
+            case 3:
+                rectTarget.transform.localPosition += new Vector3(0, rectTargetFather.sizeDelta.y / 5 * _value, 0);
+                break;
+
+            case 4:
+                rectTarget.transform.localPosition += new Vector3(0, rectTargetFather.sizeDelta.y / 5 * _value, 0);
+                break;
+            case 5:
+                rectTarget.transform.localPosition += new Vector3(0, rectTargetFather.sizeDelta.y / 5 * _value, 0);
+                break;
+           
+        }
+
+        
+      //  count += _value;
+
+      //  CheckTargetLimits();
     }
 }
