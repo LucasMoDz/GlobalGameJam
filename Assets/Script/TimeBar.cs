af07501fca72df5d16c1408dc32a2e6ed259cc07
David Fiorin <david94lozoo@gmail.com>
2017-01-21 15:27:06 +0100

    - TimeBar

---
+++ Assets/Script/TimeBar.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TimeBar : MonoBehaviour
+{
+    public Image barTime;
+    public float seconds = 60;
+    public float pieceToFill;
+
+
+    private void Start()
+    {
+        StartCoroutine(DecreaseBar());
+        pieceToFill = barTime.fillAmount / seconds;
+    }
+
+	public void DecreaseCo ()
+    {
+		StartCoroutine(DecreaseBar());
+	}
+
+    public IEnumerator DecreaseBar()
+    {
+        while (barTime.fillAmount > 0)
+        {
+            barTime.fillAmount -= pieceToFill;
+            yield return new WaitForSeconds(1);
+        }
+        yield break;
+    }
+}
