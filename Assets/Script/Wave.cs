985791622d5cc18acbec386918385ebcd853c2d2
Daniele Sangineto <scidros@gmail.com>
2017-01-21 17:33:30 +0100

    - Meccanica di richiamo del metodo UpdateBar sul player da parte dell'onda - Meccanica di far partire un audio sul neurone utilizzando le Tag Neurone e Barriera

---
+++ Assets/Script/Wave.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wave : MonoBehaviour {
+
+    public int valore = 10;
+    	
+	void OnTriggerEnter2D (Collider2D coll)
+    {
+
+
+        if (coll.gameObject.tag == "Barriera")
+        {
+            coll.transform.parent.GetComponent<WaveBarHandler>().UpdateBar(valore);
+            coll.transform.parent.GetComponent<AudioSource>().Play();
+        }
+        else if (coll.gameObject.tag == "Neurone")
+        {
+            coll.GetComponent<WaveBarHandler>().UpdateBar(valore);
+            coll.GetComponent<AudioSource>().Play();
+        }
+
+
+
+    }
+	
+	
+	void Update ()
+    {
+	
+	}
+}
