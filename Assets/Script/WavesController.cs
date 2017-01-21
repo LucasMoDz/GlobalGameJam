868c8c9744a5ac384b6c9c886bddcf447a078237
David Fiorin <david94lozoo@gmail.com>
2017-01-21 16:33:54 +0100

    - Nuova scena Animation per le animazioni

---
+++ Assets/Script/WavesController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WavesController : MonoBehaviour
+{
+    public float alpha, beta, gamma, delta, theta, defaultDamage;
+
+    // Quando ilPlaer collide con un onda, confronta il tag e aumenta il livello dell'onda cerebrale corrispondente
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "alpha":
+                alpha += defaultDamage;
+                break;
+
+            case "beta":
+                beta += defaultDamage;
+                break;
+
+            case "gamma":
+                gamma += defaultDamage;
+                break;
+
+            case "delta":
+                delta += defaultDamage;
+                break;
+
+            case "theta":
+                theta += defaultDamage;
+                break;
+        }
+       
+    }
+
+
+}
