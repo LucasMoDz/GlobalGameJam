151f1047d3a640d3f1ccf3b39db19a623c7a05eb
David Fiorin <david94lozoo@gmail.com>
2017-01-21 19:25:51 +0100

    - Sistema di animazioni completo ma non funzionante al 100%, poichè non attiva bocca sbatti

---
+++ Assets/Script/AnimatorController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimatorController : MonoBehaviour
+{
+    private Animator anim;
+    public GameObject animSbatti, animHappy, animSurprised;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetBool("isSurprised", true);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            StartCoroutine(CollisionPlayerCO());
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        StartCoroutine(CollisionPlayerCO());
+    }
+    
+    public IEnumerator CollisionPlayerCO()
+    {
+        animHappy.SetActive(false);
+        animSbatti.SetActive(true);
+        anim.SetBool("isTouched", true);
+
+
+        yield return new WaitForSeconds(1f);
+
+        animSbatti.SetActive(false);
+        anim.SetBool("isTouched", false);
+        animHappy.SetActive(true);
+    }
+}
