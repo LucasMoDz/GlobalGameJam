ca1e0f9e6c783fa7b7e33ad2e2d5ea0ff450e615
Andrea Mercuri <andrea.mercuri21@gmail.com>
2017-01-22 16:05:30 +0100

    - font e sistemati faid

--- Assets/Script/GameOver.cs
+++ Assets/Script/GameOver.cs
@@ -5,12 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour {
 
+	CanvasFader fader;
 
-	
+	public void Start() {
+		fader = FindObjectOfType<CanvasFader> ();
+	}
+
 	public void Restart ()
     {
-        SceneManager.LoadScene(0);
-
+        //SceneManager.LoadScene(0);
+		StartCoroutine(FadeToScene());
     }
 	
 	
@@ -18,5 +22,15 @@ public class GameOver : MonoBehaviour {
     {
         Application.Quit();
 	
+	}
+
+
+	public IEnumerator FadeToScene () {
+
+		fader.GetComponent<CanvasFader>().FadeOut();
+		yield return new WaitForSeconds(0.8f);
+		SceneManager.LoadScene(0);
+
+
 	}
 }
