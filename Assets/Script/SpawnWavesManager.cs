7fbb1dab23ea50989e07c9515baedb19b6fa27a5
Luca Neri <lucasmodzita@gmail.com>
2017-01-22 03:14:10 +0100

    - Manager spawn waves

--- Assets/Script/SpawnWavesManager.cs
+++ Assets/Script/SpawnWavesManager.cs
@@ -29,16 +29,38 @@ public class SpawnWavesManager : MonoBehaviour
             {
                 int value = Random.Range(0, waves.Length - 1);
                 GameObject newWave = waves[value].Spawn(generator[i].listSpawnPoints[j].firstTransform.transform.position);
-
-                Vector2 direction = generator[i].listSpawnPoints[j].secondTransform.transform.position - generator[i].listSpawnPoints[j].firstTransform.transform.position;
-                direction.Normalize();
-
-                newWave.GetComponent<Rigidbody2D>().AddForce(direction * 3, ForceMode2D.Impulse);
-
-                yield return new WaitForSeconds(Random.Range(0, 1.5f));
+                StartCoroutine(WaveMovementCO(newWave, generator[i].listSpawnPoints[j].secondTransform.transform));
             }
         }
         
+        yield break;
+    }
+
+    private IEnumerator WaveMovementCO(GameObject _wave, Transform _pointTarget)
+    {
+        _wave.SetActive(true);
+        float seconds = 0;
+        float randomValue = Random.Range(1f, 3f);
+        Vector2 startPosition = _wave.transform.position;
+        
+        while((_wave.transform.position - _pointTarget.position).magnitude > 0.2f)
+        {
+            seconds += Time.deltaTime / randomValue;
+            _wave.transform.position = Vector2.Lerp(startPosition, _pointTarget.position, seconds);
+            yield return null;
+        }
+
+        randomValue = Random.Range(3f, 6f);
+        yield return new WaitForSeconds(randomValue);
+
+        _wave.gameObject.SetActive(false);
+
+        randomValue = Random.Range(6f, 12f);
+
+        yield return new WaitForSeconds(randomValue);
+
+        _wave.transform.position = startPosition;
+        StartCoroutine(WaveMovementCO(_wave, _pointTarget));
         yield break;
     }
 }
