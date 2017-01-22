0f747bc03a5e3ece854e69ca3d2d405388b17ef0
Andrea Mercuri <andrea.mercuri21@gmail.com>
2017-01-22 12:29:05 +0100

    - Modifiche wave

--- Assets/Script/SpawnWavesManager.cs
+++ Assets/Script/SpawnWavesManager.cs
@@ -5,6 +5,7 @@ public class SpawnWavesManager : MonoBehaviour
 {
     public GameObject[] waves;
     private SpawnPointsGenerator[] generator;
+	float speed = 10;
 
     private void Start()
     {
@@ -28,10 +29,12 @@ public class SpawnWavesManager : MonoBehaviour
 
                 GameObject newWave = Instantiate(waves[value]);
                 newWave.transform.position = generator[i].listSpawnPoints[j].firstTransform.transform.position;
+				newWave.transform.LookAt(generator[i].listSpawnPoints[j].secondTransform.transform.localPosition);
 
                 if (!generator[i].listSpawnPoints[j].isSpawned)
                 {
                     StartCoroutine(WaveMovementCO(newWave, generator[i].listSpawnPoints[j].secondTransform.transform));
+					StartCoroutine(Move(newWave, generator[i].listSpawnPoints[j].secondTransform.transform));
                     generator[i].listSpawnPoints[j].isSpawned = true;
                 }
             }
@@ -47,7 +50,7 @@ public class SpawnWavesManager : MonoBehaviour
         float randomValue = Random.Range(1f, 5f);
         Vector2 startPosition = _wave.transform.position;
         
-        while((_wave.transform.position - _pointTarget.position).magnitude > 0.2f)
+		while((_wave.transform.position - _pointTarget.position).magnitude > Random.Range(1f, 2f))
         {
             seconds += Time.deltaTime / randomValue;
             _wave.transform.position = Vector2.Lerp(startPosition, _pointTarget.position, seconds);
@@ -66,4 +69,15 @@ public class SpawnWavesManager : MonoBehaviour
         StartCoroutine(WaveMovementCO(_wave, _pointTarget));
         yield break;
     }
+
+	private IEnumerator Move(GameObject _wave, Transform _pointTarget)
+	{
+		while (true) 
+		{
+			Vector3 move = _wave.transform.position - _pointTarget.position;
+			float angle = Mathf.Atan2 (move.y, move.x) * Mathf.Rad2Deg;
+			_wave.transform.rotation = Quaternion.AngleAxis (angle, Vector3.forward);
+			yield return null;
+		}
+	}
 }
